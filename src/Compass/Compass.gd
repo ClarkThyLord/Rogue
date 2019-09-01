02879ee450a6296f758f7bca22aadfee85c6624f
Christian Cuevas <ccpotential@gmail.com>
2019-09-01 10:56:57 -0700

    progress on Compass
    Clark was here :V

--- src/Compass/Compass.gd
+++ src/Compass/Compass.gd
@@ -4,13 +4,14 @@ extends Control
 
 
 # Declarations
+export(int, -180, 180) var Orientation : int = 0
 export(Vector2) var Bearing : Vector2 = Vector2()
 
 var Bearings : Array = [
 	Waypoint.new(true, 'N', null, Vector2(1, 0)),
-#	Waypoint.new(true, 'E', null, Vector2(0, 1)),
-#	Waypoint.new(true, 'S', null, Vector2(-1, 0)),
-#	Waypoint.new(true, 'W', null, Vector2(0, -1))
+	Waypoint.new(true, 'E', null, Vector2(0, 1)),
+	Waypoint.new(true, 'S', null, Vector2(-1, 0)),
+	Waypoint.new(true, 'W', null, Vector2(0, -1))
 ]
 export(bool) var BearingsVisible : bool = true
 
@@ -20,32 +21,15 @@ var Waypoints : Array = []
 func _process(delta) -> void: update()
 
 func _draw() -> void:
-	print('===')
-	print(Bearing)
-	print('***')
 	if BearingsVisible:
 		for bearing in Bearings:
-			pass
-#			rad2deg(Vector2(pp.z-tpos.z,pp.x-tpos.x).angle())
-			print(rad2deg((Bearing - (bearing as Waypoint).Position).angle()))
-#			print(rad2deg(Bearing.angle_to((bearing as Waypoint).Position)))
-	print('---')
-#		if Orientation <= 90:
-#			var pos = rect_size / 2
-#			pos.x += ((0 - Orientation) % 180) * rect_size.x / 180
-#
-#			var char_size : Vector2 = get_font('font').get_string_size('N')
-#			char_size.x = char_size.x / 2
-#			char_size.y = -char_size.y / 2
-#
-#			draw_char(get_font('font'), pos - char_size, 'N', '')
-#		elif Orientation >= 270:
-#			var pos = rect_size / 2
-#			pos.x += ((360 - Orientation) % 180) * rect_size.x / 180
-#
-#			var char_size : Vector2 = get_font('font').get_string_size('N')
-#			char_size.x = char_size.x / 2
-#			char_size.y = -char_size.y / 2
-#
-#			draw_char(get_font('font'), pos - char_size, 'N', '')
-##	draw_circle(rect_size / 2, 3, Color.blue)
+			var target : int = rad2deg(((bearing as Waypoint).Position - Bearing).angle())
+			if target >= Orientation - 90 and target <= Orientation + 90:
+				var pos = rect_size / 2
+				pos.x += ((target - Orientation) % 180) * rect_size.x / 180
+				
+				var char_size : Vector2 = get_font('font').get_string_size((bearing as Waypoint).Title)
+				char_size.x = char_size.x / 2
+				char_size.y = -char_size.y / 2
+				
+				draw_string(get_font('font'), pos - char_size, (bearing as Waypoint).Title, '')
